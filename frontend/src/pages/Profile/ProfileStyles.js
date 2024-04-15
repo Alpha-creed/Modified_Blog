7cf1d55a4eeecf8c98c7651a606fdc27a70bc622
Baidi7 <wynoral@gmail.com>
2024-04-15 02:57:26 +0000

    initial commit

--- frontend/src/pages/Profile/ProfileStyles.js
+++ frontend/src/pages/Profile/ProfileStyles.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-export const ProfileSection=styled.div`
+export const ProfileWrapper=styled.div`
     min-height:80vh;
     padding:0px 8px;
     margin-top:2rem;
@@ -12,4 +12,24 @@ export const ProfileSection=styled.div`
         align-items: flex-start; /* items-start */
         flex-direction:row;
     }
+`
+
+export const ProfPost = styled.div`
+    display:flex;
+    flex-direction:column;
+    width:100%;
+    margin-top:2rem;
+    @media screen and(max-width:768px){
+        width:70%;
+        margin-top:0rem;
+    }
+`
+
+export const H1=styled.h1`
+    font-size:3rem;
+    font-weight:bold;
+    margin-bottom:1rem;
+`
+export const ProfileSection=styled.div`
+
 `
